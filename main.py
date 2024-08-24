b3f34265e93a144d67f11f45879e3fdc95a8bffe
Sudhakar Bansal <111216638+SudhakarBansal@users.noreply.github.com>
2024-08-24 13:44:57 +0530

    initial-commit-2

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('index.html')
+
+app.run(host="127.0.0.1", port=5000, debug=True)
